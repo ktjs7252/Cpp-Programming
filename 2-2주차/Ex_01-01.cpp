76aa96878e653cf384388f5aa2bd12f6648f3cb4
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-09-29 13:36:44 +0900

    p.115 1번문제

---
+++ 2-2주차/Ex_01-01.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <array>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	string a,b; 
+	cout<<"첫번쨰 문자열 : ";
+	cin>>a;
+	cout<<"두번쨰 문자열 : ";
+	cin>>b;
+	if(a!=b)
+	{
+		cout<<"2개의 문자열은 다릅니다.\n";
+	} 
+	
+	
+	
+	
+	return 0;
+}
