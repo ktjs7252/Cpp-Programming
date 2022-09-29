886795d7621deb2091c33992f5f9d5a8228093a9
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-09-29 14:11:46 +0900

    Create Ex_01-10.cpp

---
+++ 2-2주차/Ex_01-10.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int counting=0;
+	for(int i=1;i<100;i++)
+	{
+		for(int j=1;j<100;j++)
+		{
+			for(int k=1;k<100;k++)
+			{
+				if(i*i + j*j ==k*k)
+				{
+					cout<<i<<" "<<j<<" "<<k<<"\n";
+					counting++;
+				}
+			}
+		}
+	}
+	
+	cout<<"갯수는 : "<<counting; 
+	
+	
+	return 0;
+}
